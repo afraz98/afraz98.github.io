0e57ae5918d49493173b3b07ecd7a7b430a0dfe5
afraz98 <afraz98@umd.edu>
2023-01-23 22:54:18 -0500

    Navbar font, Skill section styling

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,20 +1,25 @@
 import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDocker, faPython, faReact, faGitlab, faLinux, faJs, faUbuntu } from "@fortawesome/free-brands-svg-icons";
+import { faDocker, faPython, faReact, faGitlab, faJs, faUbuntu, faSass, faRust } from "@fortawesome/free-brands-svg-icons";
 
 class Skills extends Component {
     render() {
         return (
-            <div className="main-div" id="skills" style={{ background: "#0b0b0d", color: "white", margin: "0.2em",   display: "flex" }}>
+            <div className="skills-div" id="skills">
             <section className="div-1">
+                <section className="div-2">
                 <h1 className="heading"> Some tools that help me do what I do. </h1>
-                <a href="https://www.docker.com/"><FontAwesomeIcon icon={faDocker} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://www.python.org/"><FontAwesomeIcon icon={faPython} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://reactjs.org/"><FontAwesomeIcon icon={faReact} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://about.gitlab.com/"><FontAwesomeIcon icon={faGitlab} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://www.linux.org/"><FontAwesomeIcon icon={faLinux} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://www.javascript.com/"><FontAwesomeIcon icon={faJs} size="4x" className="fa-icon" inverse /></a>
-                <a href="https://ubuntu.com/"><FontAwesomeIcon icon={faUbuntu} size="4x" className="fa-icon" inverse /></a>
+                    <div className="d-flex justify-content-evenly">
+                    <a href="https://www.docker.com/"><FontAwesomeIcon icon={faDocker} className="fa-icon"/></a>
+                    <a href="https://www.python.org/"><FontAwesomeIcon icon={faPython} className="fa-icon"/></a>
+                    <a href="https://reactjs.org/"><FontAwesomeIcon icon={faReact} className="fa-icon"/></a>
+                    <a href="https://about.gitlab.com/"><FontAwesomeIcon icon={faGitlab} className="fa-icon"/></a>
+                    <a href="https://www.javascript.com/"><FontAwesomeIcon icon={faJs} className="fa-icon"/></a>
+                    <a href="https://ubuntu.com/"><FontAwesomeIcon icon={faUbuntu} className="fa-icon"/></a>
+                    <a href="https://sass-lang.com/"><FontAwesomeIcon icon={faSass} className="fa-icon"/></a>
+                    <a href="https://www.rust-lang.org/"><FontAwesomeIcon icon={faRust} className="fa-icon"/></a>
+                    </div>
+                </section>
             </section>
             </div>
         );
